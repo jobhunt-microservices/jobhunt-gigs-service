755cf52141eb98b7b8281a4107d2a27609e0c445
cg <congvc98@gmail.com>
2024-11-19 20:31:43 +0700

    remove stuff things

--- src/routes.ts
+++ src/routes.ts
@@ -1,12 +1,8 @@
-import { BASE_PATH } from '@gigs/constants/path';
-import { gigsRoute } from '@gigs/routes/gigs.route';
 import { healthRoutes } from '@gigs/routes/health.route';
-import { verifyGatewayRequest } from '@jobhunt-microservices/jobhunt-shared';
 import { Application } from 'express';
 
 const appRoutes = (app: Application): void => {
-  app.use(BASE_PATH, healthRoutes.routes());
-  app.use(BASE_PATH, verifyGatewayRequest, gigsRoute.routes());
+  app.use('', healthRoutes.routes());
 };
 
 export { appRoutes };
